6bbac719d13333bed6a29f890ac604e88be08132
SteveMadoerin <steve.madoerin@htwg-konstanz.de>
2023-01-20 15:19:54 +0100

    Class UnioFind completed & tests added

--- src/TelNetApplication/UnionFind.java
+++ src/TelNetApplication/UnionFind.java
@@ -1,5 +1,7 @@
 package TelNetApplication;
 
+import java.util.Arrays;
+
 /**
  * Klasse für Union-Find-Strukturen. Unterstützt die effiziente
  * Verwaltung einer Partionierung (disjunkte Zerlegung) der
@@ -8,8 +10,8 @@ package TelNetApplication;
  * */
 public class UnionFind
 {
-    private int l[];
-    private int size;
+    private int p[]; // Baume werden mit Elternfeld p dargestellt
+    private int size; // Anzahl der Mengen
 
     /**
      *Legt eine neue Union-Find-Struktur mit der Partitionierung {{0}, {1}, ..., {n-1}} an.
@@ -18,11 +20,11 @@ public class UnionFind
      *     n - Größe der Grundmenge.  */
     public UnionFind(int n)
     {
-        this.l = new int[n];
+        this.p = new int[n];
 
-        for (int i = 0; i < this.l.length; i++)
+        for (int i = 0; i < this.p.length; i++)
         {
-            this.l[i] = -1;
+            this.p[i] = -1;
         }
         this.size = n;
     }
@@ -39,11 +41,19 @@ public class UnionFind
      */
     public int find(int e)
     {
-        return 0;
+        // 10-24 Find-Algorithmus
+        while (p[e]>=0) // e ist keine Wurzel
+            e = p[e];
+
+        return e;
     }
 
     /**
-     * Vereinigt die beiden Menge s1 und s2. s1 und s2 müssen Repräsentanten der jeweiligen Menge sein. Die Vereinigung wird nur durchgeführt, falls s1 und s2 unterschiedlich sind. Es wird union-by-height durchgeführt.
+     * Vereinigt die beiden Menge s1 und s2.
+     * s1 und s2 müssen Repräsentanten der jeweiligen Menge sein.
+     * Die Vereinigung wird nur durchgeführt,
+     * falls s1 und s2 unterschiedlich sind.
+     * Es wird union-by-height durchgeführt.
      *
      * Parameters:
      *     s1 - Element, das eine Menge repräsentiert.
@@ -53,8 +63,41 @@ public class UnionFind
      */
     public void union(int s1, int s2)
     {
-        // be happy
+        // unionByHeight
+        // Baum mit kleinerer Höhe wird an Wurzel
+        // des Baums mit groesserer Hoehe gehaengt
+        // Hoehe h laesst sich bei p[e], e ist Wurzel,
+        // als negative Zahl abspeicher: p[e] = -1-h
+
+        // prüft ob s1 und s2 Repräsentanten einer Menge sind
+        if(p[s1] >= 0 || p[s2] >= 0)
+        {
+            return;
+        }
+
+        // Wenn s1 & s2 dieselbe Menge ist: do nothing.
+        if(s1 == s2)
+        {
+            return;
+        }
+        // wenn s1 weniger hoch als s2
+        if(-p[s1] < -p[s2]) // Hoehe von s1 < Hoehe von s2
+        {
+            p[s1] = s2;
+        }
+        else
+        {
+            // wenn gleichhoch
+            if(-p[s1] == -p[s2])
+                p[s1]--; // Hoehe von s1 erhoet sich um 1
+            p[s2] = s1;
+        }
+
+
+        this.size--;
+
     }
+
 
     /**
      * Liefert die Anzahl der Mengen in der Partitionierung zurück.
@@ -69,6 +112,47 @@ public class UnionFind
 
     public static void main(String[] args)
     {
+        // Testen
+        int test_size = 14;
+        UnionFind forest = new UnionFind(test_size);
+
+        forest.union(5,8);
+
+        forest.union(0,2);
+        forest.union(0,1);
+
+        forest.union(6,10);
+        forest.union(11,13);
+        forest.union(6,11);
+        forest.union(4,9);
+        forest.union(7,12);
+        forest.union(4,7);
+        forest.union(6,4);
+
+        System.out.println("8 in: " + forest.find(8));
+        System.out.println("4 in: " + forest.find(4));
+
+        System.out.println("Folie 10-28");
+        System.out.println("Amount of Partitions: " + forest.size());
+        System.out.println("Union-Find Structure: " + Arrays.toString(forest.p) + "\n");
+
+        forest.union(3,5);
+
+        System.out.println("Folie 10-28 - union(3,5)");
+        System.out.println("Amount of Partitions: " + forest.size());
+        System.out.println("Union-Find Structure: " + Arrays.toString(forest.p) + "\n");
+
+        forest.union(0, 5);
+
+        System.out.println("Folie 10-29 - union(0,5)");
+        System.out.println("Amount of Partitions: " + forest.size());
+        System.out.println("Union-Find Structure: " + Arrays.toString(forest.p) + "\n");
+
+        forest.union(0, 6);
+
+        System.out.println("Folie 10-30 - union(0,6)");
+        System.out.println("Amount of Partitions: " + forest.size());
+        System.out.println("Union-Find Structure: " + Arrays.toString(forest.p) + "\n");
 
     }
 }
